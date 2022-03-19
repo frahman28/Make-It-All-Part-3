038c3818d079079f4b082182fa898f3509eb7175
Farhan Rahman <92721094+frahman28@users.noreply.github.com>
2022-03-19 21:52:16 +0000

    Update sample data for v4 schema

--- sql/testing-data.sql
+++ sql/testing-data.sql
@@ -3,19 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 14, 2022 at 04:48 PM
+-- Generation Time: Mar 19, 2022 at 09:51 PM
 -- Server version: 5.5.68-MariaDB
--- PHP Version: 8.0.16
+-- PHP Version: 8.0.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `teamb015`
@@ -25,220 +19,231 @@ SET time_zone = "+00:00";
 -- Dumping data for table `comments`
 --
 
-INSERT INTO `comments` (`id`, `problem_id`, `author`, `comment`) VALUES
-(1, 2, 2, 'Clear cache.'),
-(2, 1, 1, 'Submerge in bag of rice for 24 hours.');
+INSERT INTO `comments` (`comment_id`, `problem_id`, `author`, `comment`) VALUES
+(1, 1, 126711, 'Submerge in bag of rice for 24 hours.'),
+(2, 2, 228787, 'Clear Cache.'),
+(3, 3, 329922, 'Update bluetooth drivers.'),
+(4, 2, 228787, 'Update windows version.');
 
 --
 -- Dumping data for table `company_roles`
 --
 
-INSERT INTO `company_roles` (`role`) VALUES
-('Administrator'),
-('Adviser'),
-('Employee'),
-('Manager'),
-('Specialist');
+INSERT INTO `company_roles` (`role_id`, `role`) VALUES
+(1, 'Administrator'),
+(2, 'Adviser'),
+(3, 'Employee'),
+(4, 'Manager'),
+(5, 'Specialist');
 
 --
 -- Dumping data for table `departments`
 --
 
-INSERT INTO `departments` (`name`) VALUES
-('Employee'),
-('IT'),
-('Managing'),
-('Operations');
+INSERT INTO `departments` (`department_id`, `name`) VALUES
+(1, 'Employee'),
+(2, 'IT'),
+(3, 'Managing'),
+(4, 'Operations');
 
 --
 -- Dumping data for table `employees`
 --
 
-INSERT INTO `employees` (`id`, `name`, `role`, `extension`) VALUES
-(1, 'Bert Thompson', 'Specialist', 101),
-(2, 'Clara Hart', 'Specialist', 102),
-(3, 'Nick Jefferson', 'Specialist', 103),
-(4, 'Terry Perry', 'Adviser', 104),
-(5, 'Dave Davidson', 'Adviser', 105),
-(6, 'Lisa Poole', 'Employee', 106),
-(7, 'Johnny Ipkiss', 'Employee', 107);
+INSERT INTO `employees` (`id`, `name`, `role_id`, `extension`) VALUES
+(126711, 'Bert Thompson', 5, '101'),
+(228787, 'Clara Hart', 5, '102'),
+(329922, 'Nick Jefferson', 5, '103'),
+(400393, 'Terry Perry', 2, '104'),
+(573827, 'Dave Davidson', 2, '105'),
+(609093, 'Lisa Poole', 3, '106'),
+(737277, 'Johnny Ipkiss', 3, '107');
 
 --
 -- Dumping data for table `employee_problem_type_relation`
 --
 
-INSERT INTO `employee_problem_type_relation` (`id`, `problem_type`) VALUES
-(1, 'Hardware'),
-(2, 'Software'),
-(3, 'Network');
+INSERT INTO `employee_problem_type_relation` (`id`, `problem_type_id`) VALUES
+(126711, 1),
+(228787, 2),
+(329922, 3);
 
 --
 -- Dumping data for table `hardware`
 --
 
-INSERT INTO `hardware` (`name`, `type`) VALUES
-('Jabra Talk 15', 'Headset'),
-('Lenovo LP7', 'Headset'),
-('iPad Air', 'iPad'),
-('iPad Pro ', 'iPad'),
-('Asus Chromebook', 'Laptop'),
-('MacBook Air', 'Laptop'),
-('MacBook Pro', 'Laptop'),
-('Microsoft Surface Pro 7', 'Laptop'),
-('Dell Inspiron', 'PC'),
-('HP Pavilion', 'PC'),
-('Canon Pixma', 'Printer'),
-('HP ENVY', 'Printer'),
-('Galaxy Tab S8', 'Tablet'),
-('LENOVO Tab M10', 'Tablet');
+INSERT INTO `hardware` (`id`, `name`, `type_id`) VALUES
+(1, 'Jabra Talk 15', 1),
+(2, 'Lenovo LP7', 1),
+(3, 'iPad Air', 2),
+(4, 'iPad Pro ', 2),
+(5, 'Asus Chromebook', 3),
+(6, 'MacBook Air', 3),
+(7, 'MacBook Pro', 3),
+(8, 'Microsoft Surface Pro 7', 3),
+(9, 'Dell Inspiron', 4),
+(10, 'HP Pavilion', 4),
+(11, 'Canon Pixma', 5),
+(12, 'HP ENVY', 5),
+(13, 'Galaxy Tab S8', 6),
+(14, 'LENOVO Tab M10', 6);
 
 --
 -- Dumping data for table `hardware_relation`
 --
 
-INSERT INTO `hardware_relation` (`id`, `name`, `employee_id`, `serial`) VALUES
-(1, 'Asus Chromebook', 6, '127827'),
-(2, 'MacBook Pro', 7, '726188'),
-(3, 'Lenovo LP7', 7, '898181'),
-(4, 'HP ENVY', 6, '919882'),
-(5, 'Dell Inspiron', 7, '299388'),
-(6, 'Dell Inspiron', 6, '635635');
+INSERT INTO `hardware_relation` (`id`, `serial`) VALUES
+(1, '127827'),
+(2, '726188'),
+(3, '898181'),
+(4, '919882'),
+(5, '299388'),
+(6, '635635'),
+(7, '272718'),
+(8, '127827'),
+(9, '726188'),
+(10, '898181'),
+(11, '919882'),
+(12, '299388'),
+(13, '635635'),
+(14, '288738');
 
 --
 -- Dumping data for table `job_info`
 --
 
-INSERT INTO `job_info` (`id`, `department`, `job_title`) VALUES
-(1, 'IT', 'Technical Specialist'),
-(2, 'IT', 'Technical Specialist'),
-(3, 'IT', 'Technical Specialist'),
-(4, 'IT', 'Technical Adviser'),
-(6, 'Employee', 'Employee'),
-(7, 'Employee', 'Employee');
+INSERT INTO `job_info` (`id`, `title_id`, `department_id`) VALUES
+(126711, 5, 2),
+(228787, 5, 2),
+(329922, 5, 2),
+(400393, 4, 2),
+(573827, 4, 2),
+(609093, 1, 1),
+(737277, 1, 1);
 
 --
 -- Dumping data for table `job_title`
 --
 
-INSERT INTO `job_title` (`title`) VALUES
-('Employee'),
-('Operations Manager'),
-('Senior Manager'),
-('Technical Adviser'),
-('Technical Specialist');
+INSERT INTO `job_title` (`title_id`, `title`) VALUES
+(1, 'Employee'),
+(2, 'Operations Manager'),
+(3, 'Senior Manager'),
+(4, 'Technical Adviser'),
+(5, 'Technical Specialist');
 
 --
 -- Dumping data for table `login_info`
 --
 
 INSERT INTO `login_info` (`id`, `password`) VALUES
-(1, 'specialist'),
-(2, 'specialist'),
-(3, 'specialist'),
-(4, 'adviser'),
-(6, 'employee'),
-(7, 'employee');
+(126711, 'specialist'),
+(228787, 'specialist'),
+(329922, 'specialist'),
+(400393, 'adviser'),
+(573827, 'adviser'),
+(609093, 'employee'),
+(737277, 'employee');
 
 --
 -- Dumping data for table `os`
 --
 
-INSERT INTO `os` (`name`) VALUES
-('Android'),
-('iOS 15'),
-('macOS 12'),
-('Windows 10'),
-('Windows 11'),
-('Windows 8');
+INSERT INTO `os` (`id`, `name`) VALUES
+(1, 'Android'),
+(2, 'iOS 15'),
+(3, 'macOS 12'),
+(4, 'Windows 10'),
+(5, 'Windows 11'),
+(6, 'Windows 8');
 
 --
 -- Dumping data for table `problems`
 --
 
-INSERT INTO `problems` (`id`, `name`, `hardware`, `software`, `os`, `last_reviewed_by`, `employee`, `assigned_to`, `problem_type`, `solved`, `closed`, `closed_on`, `opened_on`) VALUES
-(1, 'Dropped laptop in water.', 1, NULL, 'Windows 10', 1, 6, NULL, 'Hardware', 1, 1, '2022-03-06', '2022-03-02'),
-(2, 'Chrome keeps freezing.', 6, 2, 'Windows 10', 2, 7, NULL, 'Software', 1, 1, '2022-03-10', '2022-02-22'),
-(3, 'Bluetooth not connecting to laptop.', 2, NULL, 'macOS 12', NULL, 7, 3, 'Network', 0, 0, NULL, '2022-03-02'),
-(4, 'Computer not booting up.', 6, NULL, 'Windows 11', 1, 6, 4, 'Hardware', 0, 0, NULL, '2022-03-01'),
-(5, 'Application crashes frequently.', 2, 1, 'macOS 12', NULL, 7, 2, 'Software', 0, 0, NULL, '2022-02-27');
+INSERT INTO `problems` (`id`, `name`, `problem_type_id`, `software_id`, `hardware_id`, `license`, `serial`, `last_reviewed_by`, `employee`, `assigned_to`, `solved`, `closed`, `closed_on`, `opened_on`, `os_id`) VALUES
+(1, 'Dropped laptop in water.', 1, NULL, 5, NULL, '299388', 126711, 609093, NULL, 1, 1, '2022-03-15', '2022-03-09', 4),
+(2, 'Chrome keeps freezing.', 2, 1, 9, '122611', '726188', 228787, 737277, NULL, 1, 1, '2022-03-09', '2022-03-03', 6),
+(3, 'Bluetooth not connecting to laptop.', 3, NULL, 1, NULL, '127827', NULL, 737277, 329922, 0, 0, NULL, '2022-03-09', 5),
+(4, 'Computer not booting up.', 1, NULL, 9, NULL, '726188', NULL, 609093, 126711, 0, 0, NULL, '2022-03-09', 5),
+(5, 'Application crashes frequently.', 2, 2, 4, '898282', '919882', NULL, 609093, 228787, 0, 0, NULL, '2022-03-04', 2);
 
 --
 -- Dumping data for table `problem_status`
 --
 
-INSERT INTO `problem_status` (`status`) VALUES
-('Awaiting support'),
-('Comments recieved'),
-('Pending solution');
+INSERT INTO `problem_status` (`status_id`, `status`) VALUES
+(1, 'Awaiting support'),
+(2, 'Comments recieved'),
+(3, 'Pending solution');
 
 --
 -- Dumping data for table `problem_status_relation`
 --
 
-INSERT INTO `problem_status_relation` (`problem_id`, `status`) VALUES
-(3, 'Awaiting support'),
-(4, 'Awaiting support'),
-(5, 'Awaiting support');
+INSERT INTO `problem_status_relation` (`problem_id`, `status_id`) VALUES
+(4, 1),
+(5, 1),
+(3, 2);
 
 --
 -- Dumping data for table `problem_types`
 --
 
-INSERT INTO `problem_types` (`problem_type`, `level`) VALUES
-('Hardware', 1),
-('Network', 1),
-('Software', 1);
+INSERT INTO `problem_types` (`problem_type_id`, `problem_type`, `child_of`) VALUES
+(1, 'Hardware', NULL),
+(2, 'Software', NULL),
+(3, 'Network', NULL);
 
 --
 -- Dumping data for table `software`
 --
 
-INSERT INTO `software` (`name`, `type`) VALUES
-('Google Chrome', 'Application'),
-('Microsoft Excel', 'Application'),
-('Microsoft Teams', 'Application'),
-('Microsoft Word', 'Application'),
-('Safari', 'Application');
+INSERT INTO `software` (`id`, `name`, `type_id`) VALUES
+(1, 'Google Chrome', 1),
+(2, 'Microsoft Excel', 1),
+(3, 'Microsoft Teams', 1),
+(4, 'Microsoft Word', 1),
+(5, 'Safari', 1);
 
 --
 -- Dumping data for table `software_relation`
 --
 
-INSERT INTO `software_relation` (`id`, `name`, `employee_id`, `license`) VALUES
-(1, 'Microsoft Word', 7, 'GMWKB'),
-(2, 'Google Chrome', 6, 'Freeware');
+INSERT INTO `software_relation` (`id`, `license`) VALUES
+(1, '122611'),
+(2, '898282'),
+(3, '881821'),
+(4, '909201'),
+(5, '029100');
 
 --
 -- Dumping data for table `solutions`
 --
 
 INSERT INTO `solutions` (`problem_id`, `comment_id`) VALUES
-(2, 1),
-(1, 2);
+(1, 1),
+(2, 4);
 
 --
 -- Dumping data for table `type_of_hardware`
 --
 
-INSERT INTO `type_of_hardware` (`type`) VALUES
-('Headset'),
-('iPad'),
-('Laptop'),
-('PC'),
-('Printer'),
-('Tablet');
+INSERT INTO `type_of_hardware` (`type_id`, `type`) VALUES
+(1, 'Headset'),
+(2, 'iPad'),
+(3, 'Laptop'),
+(4, 'PC'),
+(5, 'Printer'),
+(6, 'Tablet');
 
 --
 -- Dumping data for table `type_of_software`
 --
 
-INSERT INTO `type_of_software` (`type`) VALUES
-('Application'),
-('Driver'),
-('Firmware'),
-('Operating System');
+INSERT INTO `type_of_software` (`type_id`, `type`) VALUES
+(1, 'Application'),
+(2, 'Driver'),
+(3, 'Firmware'),
+(4, 'Operating System');
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
