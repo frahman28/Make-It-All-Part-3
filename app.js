7ad874ab391c3172a88e68c338ef65b1992348bd
Weronika Żak <weronikarzak@gmail.com>
2022-03-22 12:37:24 +0000

    Add cors and session

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+var createError  = require('http-errors');
+var express      = require('express');
+var path         = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var ejs = require('ejs');
+var morgan       = require("morgan");
+var helmet       = require("helmet");
+var cors         = require("cors");
+var ejs          = require('ejs');
+const session    = require('express-session');
 
-// uncomment if want to reset the database
-const c          = require('./dbcreate');
+const c          = require("./dbcreate");
 
 var indexRouter  = require('./routes/auth');
 var usersRouter  = require('./routes/employees');
@@ -16,15 +18,34 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
 app.use(express.json());
+
+// for better display in the terminal
+app.use(morgan("common"));
+
+// for HTTP protection
+app.use(helmet());
+
+// for cross-origin sources
+app.use(cors());
+
 app.use(express.urlencoded({ extended: false }));
+
+// cookie parser middleware
 app.use(cookieParser());
+
+// images
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+	secret: 'team015-make-it-all-2022',
+	resave: true,
+	saveUninitialized: true
+}));
+
+// Add middleware
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/employee', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
