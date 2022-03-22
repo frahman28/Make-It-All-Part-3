0dad3ad3d48f0dbff12999681b53f4ff35ad3a6a
Weronika Żak <weronikarzak@gmail.com>
2022-03-22 12:22:15 +0000

    Change view engine to ejs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var ejs = require('ejs');
 
 // uncomment if want to reset the database
 const c          = require('./dbcreate');
@@ -14,7 +15,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
