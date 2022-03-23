ceac46b7a6047bb9e743e556e46b8b92f702ea7e
Tobias <40802800+AmzingTobuscus@users.noreply.github.com>
2022-03-23 19:31:43 +0000

    Added support for getting all employees in the database

--- routes/employees.js
+++ routes/employees.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+var conn = require("../dbconfig");
 
 /* GET users listing. */
-router.get('/dashboard', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/dashboard", (req, res, next) => {
+  res.send("respond with a resource");
+});
+
+router.get("/api/register", (req, res) => {
+  conn.query("SELECT * FROM employees", function (err, results) {
+    if (err) throw err;
+    res.send(results);
+  });
 });
 
 module.exports = router;
