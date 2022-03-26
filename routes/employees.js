363a1f176afc892c6f2b97d6bfc1d27c8c548237
Tobias <40802800+AmzingTobuscus@users.noreply.github.com>
2022-03-26 22:59:43 +0000

    Added support for not updating all fields in the employee table

--- routes/employees.js
+++ routes/employees.js
@@ -8,18 +8,19 @@ router.get("/dashboard", (req, res, next) => {
   res.send("respond with a resource");
 });
 
-router.get("/api/register", (req, res) => {
+router.get("/api", (req, res) => {
   conn.query("SELECT * FROM employees", function (err, results) {
     if (err) throw err;
     res.send(results);
   });
 });
 
-router.get("/api/edit/:employee_id", (req, res) => {
-  if (req.params.employee_id) {
+router.get("/api/:employee_id", (req, res) => {
+  const employeeID = req.params.employee_id;
+  if (employeeID) {
     conn.query(
       "SELECT * FROM employees WHERE employee_id = ?",
-      req.params.employee_id,
+      employeeID,
       function (err, results) {
         if (err) throw err;
         res.send(results);
@@ -30,30 +31,63 @@ router.get("/api/edit/:employee_id", (req, res) => {
   }
 });
 
-router.put("/api/update/:employee_id", (req, res) => {
-  const toUpdateWith = {
-    employee_id: req.params.employee_id,
-    name: req.body.name,
-    role_id: req.body.role_id,
-    extension: req.body.extension,
-    external: req.body.external,
-    available: req.body.available,
-  };
+router.put("/api/:employee_id", (req, res) => {
+  console.log(req.body);
+  let { name, extension, external, available } = req.body;
+  console.log(name, extension, external, available);
+  const employeeID = req.params.employee_id;
+  console.log("I'm here");
   conn.query(
-    "UPDATE employees SET ? WHERE employee_id = ?",
-    [toUpdateWith, toUpdateWith.employee_id],
+    "SELECT * FROM employees WHERE employee_id = ?",
+    employeeID,
     function (err, results) {
+      console.log(name, extension, external, available);
+
+      if (results.length < 1) {
+        return res.json({ success: false, msg: "Employee not found" });
+      }
       if (err) throw err;
-      res.send("Updated");
+      if (name === undefined) {
+        name = results[0].name;
+      }
+      if (extension === undefined) {
+        extension = results[0].extension;
+      }
+      if (external === undefined) {
+        if (!Number.isInteger(available)) {
+          external = results[0].external;
+        }
+      }
+      if (available === undefined) {
+        if (!Number.isInteger(available)) {
+          available = results[0].available;
+        }
+      }
+      const toUpdateWith = {
+        employee_id: employeeID,
+        name: name,
+        extension: extension,
+        external: external,
+        available: available,
+      };
+      conn.query(
+        "UPDATE employees SET ? WHERE employee_id = ?",
+        [toUpdateWith, toUpdateWith.employee_id],
+        function (err, results) {
+          if (err) throw err;
+          res.json({ success: true, msg: "Employee updated" });
+        }
+      );
     }
   );
 });
 
-router.delete("/api/:id", (req, res) => {
-  if (req.params.id) {
+router.delete("/api/:employee_id", (req, res) => {
+  const employeeID = req.params.employee_id;
+  if (employeeID) {
     conn.query(
       "DELETE FROM employees WHERE employee_id = ?",
-      req.params.id,
+      employeeID,
       function (err) {
         if (err) throw err;
         res.send("Deleted");
