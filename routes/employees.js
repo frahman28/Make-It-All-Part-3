daa62a20cf92a6a6c6f6f8f0ad6281edb1aca927
Tobias <40802800+AmzingTobuscus@users.noreply.github.com>
2022-03-27 11:05:35 +0100

    Changed the method for updating employee details

--- routes/employees.js
+++ routes/employees.js
@@ -54,49 +54,39 @@ router.get("/api/:employee_id", (req, res) => {
 router.put("/api/:employee_id", (req, res) => {
   let { name, extension, external, available } = req.body;
   const employeeID = req.params.employee_id;
-  conn.query(
-    "SELECT * FROM employees WHERE employee_id = ?",
-    employeeID,
-    function (err, results) {
-      console.log(name, extension, external, available);
-
-      if (results.length < 1) {
-        return res.json({ success: false, msg: "Employee not found" });
-      }
-      if (err) throw err;
-      if (name === undefined) {
-        name = results[0].name;
-      }
-      if (extension === undefined) {
-        extension = results[0].extension;
-      }
-      if (external === undefined) {
-        if (!Number.isInteger(available)) {
-          external = results[0].external;
-        }
-      }
-      if (available === undefined) {
-        if (!Number.isInteger(available)) {
-          available = results[0].available;
-        }
-      }
-      const toUpdateWith = {
-        employee_id: employeeID,
-        name: name,
-        extension: extension,
-        external: external,
-        available: available,
-      };
-      conn.query(
-        "UPDATE employees SET ? WHERE employee_id = ?",
-        [toUpdateWith, toUpdateWith.employee_id],
-        function (err, results) {
-          if (err) throw err;
-          return res.json({ success: true, msg: "Employee updated" });
-        }
-      );
+  let toUpdateWith = {};
+  if (name !== undefined) {
+    toUpdateWith.name = name;
+  }
+  if (extension !== undefined) {
+    toUpdateWith.extension = extension;
+  }
+  if (external !== undefined) {
+    if (Number.isInteger(external)) {
+      toUpdateWith.external = external;
     }
-  );
+  }
+  if (available !== undefined) {
+    if (Number.isInteger(available)) {
+      toUpdateWith.available = available;
+    }
+  }
+  if (Object.keys(toUpdateWith).length > 0) {
+    conn.query(
+      "UPDATE employees SET ? WHERE employee_id = ?",
+      [toUpdateWith, employeeID],
+      function (err, results) {
+        if (err) throw err;
+        if (results.affectedRows > 0) {
+          return res.json({ success: true, msg: "Employee updated" });
+        } else {
+          return res.json({ success: false, msg: "Employee not found" });
+        }
+      }
+    );
+  } else {
+    res.json({ success: false, msg: "Invalid request" });
+  }
 });
 
 router.delete("/api/:employee_id", (req, res) => {
