a6cde3ac828cb65ca965aee577cb419df26d79d1
Tobias <40802800+AmzingTobuscus@users.noreply.github.com>
2022-03-30 16:37:11 +0100

    Add the api call for getting all problem types

--- routes/problem-type.js
+++ routes/problem-type.js
@@ -3,4 +3,12 @@ const router = express.Router();
 
 var conn = require("../dbconfig");
 
+router.get("/api", (req, res) => {
+  // This API call will return the list of all problem types in the database
+  conn.query("SELECT * FROM problem_types", (err, results) => {
+    if (err) throw err;
+    return res.json({ success: true, data: results });
+  });
+});
+
 module.exports = router;
