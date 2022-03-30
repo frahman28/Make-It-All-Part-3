09029ce991a999a854fa8b45a464efc98f3db6fc
Tobias <40802800+AmzingTobuscus@users.noreply.github.com>
2022-03-30 16:50:36 +0100

    Add api support for getting all problem types that are children of another problem type

--- routes/problem-type.js
+++ routes/problem-type.js
@@ -11,4 +11,21 @@ router.get("/api", (req, res) => {
   });
 });
 
+router.get("/api/children", (req, res) => {
+  // This API call will return the list of children for a specific problem type,
+  // the problem type is based on the request body supplied
+  const { problemTypeID } = req.body;
+  if (problemTypeID === undefined || !Number.isInteger(problemTypeID)) {
+    return res.json({ success: false, msg: "Problem type id not supplied" });
+  }
+  conn.query(
+    "SELECT * FROM problem_types WHERE child_of = ?",
+    problemTypeID,
+    (err, results) => {
+      if (err) throw err;
+      return res.json({ success: true, data: results });
+    }
+  );
+});
+
 module.exports = router;
