0c5cc50e63528dc40d7992923259274149cd5bb5
Alec Holden <alec.s.holden@gmail.com>
2022-04-09 22:08:07 +0100

    Add table dropdown functionality

--- public/js/all_problems.js
+++ public/js/all_problems.js
@@ -1,8 +1,26 @@
 //Used to colour the data in status row
-$("tr td:last-child").each(function(i) {
-    if ($(this).text() == "Solved") {
-        $(".status").eq(i).css({"background-color":"#62cc3d"});
-    } else if ($(this).text() == "Closed") {
-        $(".status").eq(i).css({"background-color":"#ff4f4f"});
+$(".visible-row td:last-child").each(function (i) {
+    if ($(this).text().trim() == "Solved") {
+        $(".status").eq(i).css({ "background-color": "#62cc3d" });
+    } else if ($(this).text().trim() == "Closed") {
+        $(".status").eq(i).css({ "background-color": "#ff4f4f" });
     }
+});
+
+$(document).ready(function () {
+    //Change colour of row on hover
+    $(".visible-row").hover(function () {
+        $(this).find("td:nth-child(n)").css("background-color", "#666564");
+        $(this).css('cursor', 'pointer');
+    }, function () {
+        $(this).find("td:nth-child(n)").css("background-color", "#ffffff");
+        $(this).css('cursor', 'default');
+    });
+
+    //Collapse rows seperately
+    $(".visible-row").click(function () {
+        $(this).find("td:nth-child(n)").css("background-color", "#ffffff");
+        $(this).next("tr").collapse("toggle");
+        $(this).next().find("div").collapse("toggle");
+    });
 });
