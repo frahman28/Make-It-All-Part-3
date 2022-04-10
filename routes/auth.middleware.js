e35348f266d9228794cfd77013529bf5e752b289
Weronika Żak <weronikarzak@gmail.com>
2022-04-10 22:45:08 +0100

    Rename the auth middleware

---
+++ routes/auth.middleware.js
@@ -0,0 +1,41 @@
+// auth.middleware.js
+// Stores callbacks to handl user authorization and
+// authentication.
+
+
+// Callback 
+// Checks whether a session instance exists.
+// (if there is any user of any role logged in). 
+var verifySession = function(req, res, next) {
+    // If no session exists, throw an error.
+    if (!req.session) {
+        return req.sendStatus(403);
+    }
+    else {
+        // Otherwise allow for running the function..
+        next();
+    }
+};
+
+
+// Callback 
+// Checks whether a session instance exists.
+// (if there is any user of any role logged in). 
+function checkRoles(...permittedRoles) {
+    // Return a middleware.
+    return (req, res, next) => {
+        // If no session exists, throw an error.
+        if (!req.session) return res.sendStatus(403);
+        
+        // If session exits and current user has requested role,
+        // allow for running the function.
+        if (req.session && permittedRoles.includes(req.session.userRole)) {
+            next();
+        } else {
+            // Otherwise throw an error.
+            return res.sendStatus(401);
+        }
+    }
+}
+
+module.exports =  {verifySession, checkRoles};
