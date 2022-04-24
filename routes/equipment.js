f8b7118b6656986484a13bd47ef4ba7983781d99
Farhan Rahman <92721094+frahman28@users.noreply.github.com>
2022-04-24 10:54:13 +0100

    Add types to get by id route

--- routes/equipment.js
+++ routes/equipment.js
@@ -4,9 +4,9 @@ var router = express.Router();
 var conn = require('../config');
 
 //Import all functions from hardware, software, os files to interact with database
-var {getAllHardware, getHardwareTypes, getHardwareById, addHardware, updateHardware, deleteHardware} = require('./hardware');
-var {getAllSoftware, getSoftwareTypes, getSoftwareById, addSoftware, updateSoftware, deleteSoftware} = require('./software');
-var {getAllOS, getOSById, addOS, updateOS, deleteOS} = require('./os');
+var {getAllHardware, getHardwareTypes, getHardwareById, addHardware, updateHardware, deleteHardware} = require('./hware');
+var {getAllSoftware, getSoftwareTypes, getSoftwareById, addSoftware, updateSoftware, deleteSoftware} = require('./sware');
+var {getAllOS, getOSById, addOS, updateOS, deleteOS} = require('./osys');
 
 
 //Single get route calls functions from all files to display results on single page
@@ -24,10 +24,12 @@ router.get('/viewEquipment', async function(req, res) {
 //Single get route calls functions from all files to display results on single page
 router.get('/viewEquipment/:id', async function(req, res) {
     var hware = await getHardwareById(req);
+    var hwareTypes = await getHardwareTypes();
     var sware = await getSoftwareById(req);
+    var swareTypes = await getSoftwareTypes();
     var osys = await getOSById(req);
 
-    res.render("viewEquipment", { hardware: hware, software: sware, os: osys });
+    res.render("viewEquipment", { hardware: hware, hardwareTypes: hwareTypes, software: sware, softwareTypes: swareTypes, os: osys });
 });
 
 //Post route specific to adding hardware, calls addHardware function
