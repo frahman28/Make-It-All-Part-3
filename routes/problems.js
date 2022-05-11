72467da6064e6ec1b1e58d8596afd74fb7941fc7
Weronika Żak <weronikarzak@gmail.com>
2022-05-12 00:49:35 +0100

    Reassign specialist or mark problem as resolved

--- routes/problems.js
+++ routes/problems.js
@@ -68,6 +68,7 @@ app.get('/myProblems', checkRoles("specialist", "employee"), async function(req,
                     opened_on as dateOpened,
                     closed_on as dateClosed,
                     problems.os_id as problemOSId,
+                    solved,
                     status
                 FROM problems
                 LEFT JOIN employees 
@@ -119,9 +120,12 @@ app.all('/allProblems', checkRoles("specialist", "employee", "admin"), function 
                     opened_on as dateOpened,
                     closed_on as dateClosed,
                     status,
+                    closed,
                     os.name as OS,
                     software.name as softwareName,
                     type_of_software.type as softwareType,
+                    solut.comment as solution,
+                    comments.comment as solutionNotes,
                     hardware.name as hardwareName,
                     type_of_hardware.type as hardwareType,
                     hardware_relation.serial as serialNumber
@@ -146,6 +150,13 @@ app.all('/allProblems', checkRoles("specialist", "employee", "admin"), function 
                     ON problems.problem_id = problem_status_relation.problem_id
                 LEFT JOIN problem_status
                     ON problem_status_relation.status_id = problem_status.status_id
+                LEFT JOIN solutions 
+                    ON solutions.problem_id = problems.problem_id
+                LEFT JOIN comments AS solut 
+                    ON solut.comment_id = solutions.comment_id
+                LEFT JOIN comments 
+                    ON comments.problem_id = problems.problem_id 
+                    AND comments.comment_id NOT IN (select comment_id FROM solutions)
                 ORDER BY problems.problem_id ASC;`, function (err, rows) {
         if (err) {
         // If error occured, return an empty array.
@@ -188,6 +199,8 @@ app.post("/submitProblem", checkRoles("employee", "specialist"), async function 
     let license = req.body.license.length > 0 ? req.body.license : null;
     let serialNumber = req.body.serialNumber.length > 0 ? req.body.serialNumber : null;
     let problemDesription = req.body.problemDesription;
+    let solution = req.body.solution;
+    let solutionNotes = req.body.solutionNotes;
 
     console.log(req.body);
     
@@ -200,7 +213,7 @@ app.post("/submitProblem", checkRoles("employee", "specialist"), async function 
 
     let specialistsForProblemType = await problemTypes.getListOfSpecialistForProblemType(problemType, true);
     if (specialistsForProblemType.length < 1) {
-        let problemParent = await getChildNodeID(problemType);
+        let problemParent = await problemTypes.getChildNodeID(problemType);
         let specialistsForProblemType = await problemTypes.getListOfSpecialistForProblemType(problemParent, true);
 
         if (specialistsForProblemType.length < 1) {
@@ -220,11 +233,7 @@ app.post("/submitProblem", checkRoles("employee", "specialist"), async function 
         serialNumber, req.session.userId, asssignedSpecialist, 
         openedOn, operatingSystem);
 
-    console.log(newProblemId.insertId)
     await problemUtils.createProblemStatus(newProblemId.insertId);
-
-    let solution = req.body.solution;
-    let solutionNotes = req.body.solutionNotes;
 
     if (solution.length > 0) {
         await problemUtils.updateProblemStatus(newProblemId.insertId, 3);
@@ -243,7 +252,7 @@ app.post("/submitProblem", checkRoles("employee", "specialist"), async function 
 
 
 
-app.get("/submitProblem/:problemId", checkRoles("employee", "specialist", "admin"), async function (req, res, next) {
+app.get("/submitProblem/:problemId", checkRoles("employee", "specialist"), async function (req, res, next) {
     let problemId = req.params["problemId"];
     if (isNaN(problemId)) return res.redirect("../myProblems");
 
@@ -259,11 +268,15 @@ app.get("/submitProblem/:problemId", checkRoles("employee", "specialist", "admin
     }
 
     // change problem status
-
-    if (problem[0].lastReviewedBy != req.session.userId) {
+    if (problem[0].lastReviewedBy != req.session.userId && req.session.userRole == "specialist") {
         await problemUtils.updateProblemLastViewedBy(problemId, req.session.userId);
+        await problemUtils.updateProblemStatus(problemId, 2);
     }
-    await problemUtils.updateProblemStatus(problemId, 2);
+    
+    let problemSolution = undefined;
+    if (problem[0].solved == 1) {
+        problemSolution = await solutionUtils.getSolutionForProblemId(problemId);
+    }
 
     var allSoftware = await software.getAllSoftware();
     var allHardware = await hardware.getAllHardware();
@@ -271,18 +284,19 @@ app.get("/submitProblem/:problemId", checkRoles("employee", "specialist", "admin
     var allSolutions = await solutionUtils.getAllSolutions();
     var allProblemTypes = await problemTypes.getAllProblemTypes();
 
-    res.render('submitProblem', {userName: req.session.userName,
+    return res.render('submitProblem', {userName: req.session.userName,
                                 problem: problem,
                                 software: allSoftware,
                                 hardware: allHardware,
                                 os: allOS,
                                 solution: allSolutions,
+                                problemSolution: problemSolution,
                                 problemTypes: allProblemTypes,
                                 role: req.session.userRole});
 });
 
 
-app.post("/submitProblem/:problemId", checkRoles("employee", "specialist", "admin"), async function (req, res, next) {
+app.post("/submitProblem/:problemId", checkRoles("employee", "specialist"), async function (req, res, next) {
     let problemId = req.params["problemId"];
     let author = req.session.userId;
     let solution = req.body.solution;
@@ -293,13 +307,85 @@ app.post("/submitProblem/:problemId", checkRoles("employee", "specialist", "admi
     let newSolution = await solutionUtils.addComments(problemId, author, solution);
     await solutionUtils.linkProblemToSolution(problemId, newSolution.insertId);
 
-    if (solution.length < 1) {
+    if (solutionNotes.length > 0) {
         await solutionUtils.addComments(problemId, author, solutionNotes);
     }
     
     await problemUtils.updateProblemStatus(problemId, 3);
-    // await problemUtils.updateProblem(problemId);
+    await problemUtils.setProblemSolved(problemId, 1);
+
     return res.redirect('/myProblems');
+});
+
+
+app.get("/reassignProblem/:problemId", checkRoles("employee", "specialist"), async function (req, res, next) {
+    // TODO
+    let problemId = req.params["problemId"];
+    if (isNaN(problemId)) return res.redirect("../myProblems");
+
+    let problem = await problemUtils.getProblemById(problemId);
+
+    if (problem.length < 1) {
+        return res.redirect("../myProblems");
+    }
+
+    if (problem[0].reportedBy != req.session.userId) {
+        // Prohibit enter.
+        return res.sendStatus(401);
+    }
+
+    let asssignedSpecialist;
+    let problemType = problem[0].problemTypeId;
+    console.log("problemType, assignedSpecialist: ", problemType, problem[0].assignedSpecialist)
+
+    let specialistsForProblemType = await problemTypes.getListOfSpecialistForProblemTypeExcluding(problemType, problem[0].assignedSpecialist);
+    console.log("specialistsForProblemType: ", specialistsForProblemType)
+
+    if (specialistsForProblemType.length < 1) {
+        let problemParent = await problemTypes.getChildNodeID(problemType);
+        if (problemParent.length < 1) {
+            let allSpecialists = await problemUtils.getAllSpecialists();
+            asssignedSpecialist = allSpecialists[0].specialistId;
+        } else {
+            let specialistsForProblemType = await problemTypes.getListOfSpecialistForProblemTypeExcluding(problemParent[0].child_of, problem[0].assignedSpecialist);
+            if (specialistsForProblemType.length < 1) {
+                let allSpecialists = await problemUtils.getAllSpecialists();
+                asssignedSpecialist = allSpecialists[0].specialistId;
+    
+            } else {
+                asssignedSpecialist = specialistsForProblemType[0].specialistId;
+            }
+        }
+    } else {
+        asssignedSpecialist = specialistsForProblemType[0].specialistId;
+    }
+
+    await problemUtils.reassignSpecialist(problemId, asssignedSpecialist);
+    await problemUtils.updateProblemStatus(problemId, 1);
+    await problemUtils.setProblemSolved(problemId, 0);
+
+    return res.redirect('../myProblems');
+});
+
+app.get("/resolveProblem/:problemId", checkRoles("employee", "specialist"), async function (req, res, next) {
+    // TODO
+    let problemId = req.params["problemId"];
+    if (isNaN(problemId)) return res.redirect("../myProblems");
+
+    let problem = await problemUtils.getProblemById(problemId);
+
+    if (problem.length < 1) {
+        return res.redirect("../myProblems");
+    }
+
+    if (problem[0].reportedBy != req.session.userId) {
+        // Prohibit enter.
+        return res.sendStatus(401);
+    }
+    console.log("PROBLEM ", problemId, " RESOLVED");
+    await problemUtils.setProblemClosed(problemId);
+
+    return res.redirect('../myProblems');
 });
 
 
