e912707456799c7c33d3d2bbbeb1201e91c28cfb
Alec Holden <alec.s.holden@gmail.com>
2022-05-15 12:48:14 +0100

    Add validation to change password

---
+++ public/js/settings.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+    $('form').on('submit', function(event) {
+        var valid = true;
+        var alert = "";
+        if ($("#new-password").val().length == 0) {
+            valid = false;
+            alert = "Missing new password.";
+            console.log("test3");
+        }
+        if ($("#confirm-new-password").val().length == 0) {
+            valid = false;
+            if (alert == "") {
+                alert = "Missing confirm new password.";
+            }
+            else {
+                alert = "Missing password.";
+            }
+            
+        }
+        if (valid == true) {
+            if ($("#new-password").val() != $("#confirm-new-password").val()) {
+                valid = false;
+                alert = "Password and confirm password must be the same."
+            }
+        }
+        if (valid == true) {
+            if ($("#new-password").val().length < 6) {
+                valid = false;
+                alert = "Password must be at least 6 characters long.";
+            }
+            else if ($("#new-password").val().length > 256) {
+                valid = false;
+                alert = "Password too long.";
+            }
+        }
+        if (valid == false) {
+            event.preventDefault();
+            $(".inputValidation").text(alert);
+            $("#new-password").val("");
+            $("#confirm-new-password").val("");
+            $(".main-alerts").hide();
+            $(".inputValidation").show();
+        }
+    });
+});
