718e7fd05c676944684bba6a0b9bc048d3d2561d
Weronika Żak <weronikarzak@gmail.com>
2022-05-16 07:39:51 +0100

    Fix searching functions for problems

--- public/js/search-functions.js
+++ public/js/search-functions.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    var allRows = $(".problem-row");
+
     $("#solution-search").on("keyup", function() {
       var value = $(this).val().toLowerCase();
       $(".solution-row").filter(function() {
@@ -6,78 +8,53 @@ $(document).ready(function(){
       });
     });
 
-    $("#solved-only").change(function() {
-        if(this.checked) {
-            $(".problem-row").filter(function() {
-                $(this).toggle($(this).find(".status").text().indexOf("Solved") < 0)
-            }); 
-        } else {
-            $(".problem-row").filter(function() {
-                $(this).toggle($(this).text().indexOf("") > -1)
-            });
-        }
-    });
-
-    $("#reported-by-me").change(function() {
+    $("#search-problems-button").click(function() {
+        // $(".problem-row").each(function() {$(this).removeClass("filtered-row");});
         var currentUser = $(document).find("#current-user-id").text();
-        if(this.checked) {
-            $(".problem-row").filter(function() {
-                $(this).toggle($(this).next().find(".reported-by").text().indexOf(currentUser) > -1)
-            }); 
-        } else {
-            $(".problem-row").filter(function() {
-                $(this).toggle($(this).text().toLowerCase().indexOf("") > -1)
-            });
-        }
-    });
+        // console.log("1111", rows)
 
-    $("#assigned-to-me").change(function() {
-        var currentUser = $(document).find("#current-user-id").text();
-        if(this.checked) {
-            $(".problem-row").filter(function() {
-                $(this).toggle($(this).next().find(".specialistHidden").val().indexOf(currentUser) > -1)
-            }); 
-        } else {
-            $(".problem-row").filter(function() {
-                $(this).toggle($(this).text().toLowerCase().indexOf("") > -1)
-            });
-        }
-    });
+        $(".problem-row").each(function() {
+            $(this).removeClass("filtered-row");
+            console.log($(this))
 
-    $("#problem-search").on("keyup", function() {
-        var value = $(this).val().toLowerCase();
-        $(".problem-row").filter(function() {
+            if($("#solved-only").is(':checked')) {
+                if(($(this).find("div.status").text().indexOf("Solved")>0)) {
+                    $(this).addClass("filtered-row");
+                }
+            }
+
+            if ($("#reported-by-me").is(':checked')) {
+                if ($(this).next().find(".reported-by").text() !== currentUser) {
+                    $(this).addClass("filtered-row")
+                }
+            }
+
+            if($("#assign-to-me").is(':checked')) {
+                if ($(this).next().find(".specialistHidden").val() !== currentUser) {
+                    $(this).addClass("filtered-row")
+                }
+            }; 
+
+            var value = $("#problem-search").val().toLowerCase();
             var problemName = $(this).next().find(".problem-name").text().toLowerCase();
             var problemDescription = $(this).next().find(".problem-description").text().toLowerCase();
-          $(this).toggle((problemName + " " + problemDescription).indexOf(value) > -1)
+            if ((problemName + " " + problemDescription).indexOf(value) < 0) {
+                $(this).addClass("filtered-row")
+            }
+
+            value = $('#specialist-search').val();
+            if (value !== 'all') {
+                if ($(this).next().find(".specialistHidden").val() !== value) {
+                    $(this).addClass("filtered-row")
+                }
+            }
+
+            var value = $('#status-list').val();
+            if (value !== 'all') {
+                if(($(this).find("div.status").text().indexOf(value) < 0)) {
+                    $(this).addClass("filtered-row");
+                }
+            }
         });
-      });
-
-    $('#specialist-search').change(function() {
-        var value = $(this).val();
-        if (value === '') {
-            $(".problem-row").filter(function() {
-                $(this).toggle($(this).text().toLowerCase().indexOf("") > -1)
-            });
-        } else {
-            $(".problem-row").filter(function() {
-                $(this).toggle($(this).next().find(".specialistHidden").val().indexOf(value) > -1)
-            });
-        }
-    });
-
-    $('#status-list').change(function() {
-        var value = $(this).val();
-        if (value === '') {
-            $(".problem-row").filter(function() {
-                var status = $.trim($(this).find(".status").text());
-                $(this).toggle(status.indexOf("") > -1)
-            });
-        } else {
-            $(".problem-row").filter(function() {
-                var status = $.trim($(this).find(".status").text());
-                $(this).toggle(status.indexOf(value) > -1)
-            });
-        }
     });
 });
